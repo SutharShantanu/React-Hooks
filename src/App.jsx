314f22ce180f5f92011e02c9d14232dab6099a8e
Shantanu <shantanusut2000@gmail.com>
2023-06-08 00:12:34 +0530

    3 hooks practiced

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import "./App.css";
 import GridCard from "./Components/GridCard";
+import { FaReact } from "react-icons/fa";
 
 function App() {
     return (
         <div className="App">
-            <header></header>
+            <header>
+                <h1>React Hooks</h1>
+                <span>&nbsp;</span>
+                <FaReact className="icon" />
+            </header>
             <div className="main">
                 <GridCard
                     title="useContext Hook"
                     code="https://codesandbox.io/s/contexthook-2l9gq3"
+                    hook="ContextHook"
+                    embed="https://codesandbox.io/embed/contexthook-2l9gq3?fontsize=8&hidenavigation=1&theme=dark&view=preview&hidedevtools=1&expanddevtools=0&forcerefresh=1&"
+                    link="https://codesandbox.io/s/contexthook-2l9gq3?fontsize=14&hidenavigation=1&theme=dark"
+                />
+                <GridCard
+                    title="useReducer Hook"
+                    code="https://codesandbox.io/s/contexthook-2l9gq3"
+                    hook="ContextHook"
+                    embed="https://codesandbox.io/embed/usereducer-jmn63o?fontsize=14&hidenavigation=1&theme=dark&view=preview&hidedevtools=1&expanddevtools=0&forcerefresh=1&"
+                    link="https://codesandbox.io/s/usereducer-jmn63o?fontsize=14&hidenavigation=1&theme=dark&view=preview"
+                />
+                <GridCard
+                    title="useRef Hook"
+                    code="https://codesandbox.io/s/contexthook-2l9gq3"
+                    hook="ContextHook"
+                    embed="https://codesandbox.io/embed/useref-cpmr10?fontsize=14&hidenavigation=1&theme=dark&view=preview&hidedevtools=1&expanddevtools=0&forcerefresh=1&"
+                    link="https://codesandbox.io/s/useref-cpmr10?fontsize=14&hidenavigation=1&theme=dark&view=preview"
                 />
             </div>
         </div>
     );
 }
-
 export default App;
+
--- src/Components/GridCard.jsx
+++ src/Components/GridCard.jsx
@@ -1,29 +1,25 @@
 import React from "react";
-import { TbExternalLink, TbCode } from "react-icons/tb";
+import "./GridCard.css";
+import { RxCodesandboxLogo } from "react-icons/rx";
 
-const GridCard = ({ title, code }) => {
+const GridCard = ({ title, embed, hook, link }) => {
     return (
         <div className="main_card">
-            <h1>{title}</h1>
-            <div className="card_button">
-                <button onClick={() => window.open(code, "_blank")}>
-                    Code
-                    <TbCode />
-                </button>
-                <iframe
-                    style={{
-                        width: "100%",
-                        height: "250px",
-                        border: "0",
-                        borderRadius: "8px",
-                        overflow: "hidden",
-                        margin:"15px"
-                    }}
-                    src="https://codesandbox.io/embed/contexthook-2l9gq3?fontsize=14&hidenavigation=1&theme=dark&view=preview"
-                    title="ContextHook"
-                    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-                    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
-            </div>
+            <h3>{title}</h3>
+            <a
+                href={link}
+                className="button"
+                target="_blank"
+                rel="noopener noreferrer">
+                Edit on
+                <RxCodesandboxLogo className="button_icon" />
+            </a>
+
+            <iframe
+                src={embed}
+                title={hook}
+                allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
         </div>
     );
 };
